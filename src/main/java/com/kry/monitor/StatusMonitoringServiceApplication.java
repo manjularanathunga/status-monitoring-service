4ca76f986e3ee26851f7bfc1e2f7807c7a67d399
Manjula Ranathunga <manjula.ranathunga.sl@gmail.com>
2021-09-24 14:59:38 +0200

    Initial project commit for the github.

---
+++ src/main/java/com/kry/monitor/StatusMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kry.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusMonitoringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusMonitoringServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kry/monitor/StatusMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kry.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
